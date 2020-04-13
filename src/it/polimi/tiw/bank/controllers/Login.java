c4ee901ea2d9f50276ae25a24b37b503924dd91e
Litkovskyy Valeriy <5009900-zae5quee2eequ6jieJaeh7chie6zie@users.noreply.gitlab.com>
2020-04-13 15:52:47 +0200

    remove printwriter

--- src/it/polimi/tiw/bank/controllers/Login.java
+++ src/it/polimi/tiw/bank/controllers/Login.java
@@ -1,7 +1,6 @@
 package it.polimi.tiw.bank.controllers;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
