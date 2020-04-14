2ec43bdb47b3a4e4863cc07306d7afdab4de4e50
Alessio Spagnolo <alessio.spanish@mail.polimi.it>
2020-04-14 14:03:26 +0200

    TransferDao changes

--- src/it/polimi/tiw/bank/dao/TransferDao.java
+++ src/it/polimi/tiw/bank/dao/TransferDao.java
@@ -55,31 +55,55 @@ public class TransferDao {
 		return transfers;
 	}
 
-	public long createTransfer(
-			long sourceAccountId,
-			long destinationAccountId,
-			long amount,
-			String cause )
-		throws SQLException {
-		String query =
-            "INSERT INTO transfer (source_account_id, destination_account_id, amount, cause)\n" +
-            "SELECT ?, ?, ?, ?\n" +
-            "  FROM account_total_amount\n" +
-            " WHERE account_id = ? AND total_amount >= ?\n";
+	public long createTransfer(long sourceAccountId, long destinationAccountId, long destinationCustomerId, long amount,
+			String cause) throws SQLException {
+		String query = "SELECT customer_id FROM account WHERE account_id = ? AND customer_id = ?";
 
+		ResultSet result = null;
 		PreparedStatement pstatement = null;
+		try {
+
+			pstatement = connection.prepareStatement(query);
+			pstatement.setLong(1, destinationAccountId);
+			pstatement.setLong(2, destinationCustomerId);
+			result = pstatement.executeQuery();
+			if (!result.next()) {
+				throw new SQLException("L'account immesso non appartiene all'utente inserito");
+			}
+
+		} catch (SQLException e) {
+			throw new SQLException(e);
+
+		} finally {
+			try {
+				result.close();
+			} catch (Exception e1) {
+				throw new SQLException(e1);
+			}
+			try {
+				pstatement.close();
+			} catch (Exception e2) {
+				throw new SQLException(e2);
+			}
+		}
+
+		query = "INSERT INTO transfer (source_account_id, destination_account_id, amount, cause)\n"
+				+ "SELECT ?, ?, ?, ?\n" + "  FROM account_total_amount \n"
+				+ " WHERE account_id = ? AND total_amount >= ?\n";
+
+		pstatement = null;
 		try {
 			pstatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
 			pstatement.setLong(1, sourceAccountId);
 			pstatement.setLong(2, destinationAccountId);
 			pstatement.setLong(3, amount);
 			pstatement.setString(4, cause);
-            pstatement.setLong(5, sourceAccountId);
-            pstatement.setLong(6, amount);
+			pstatement.setLong(5, sourceAccountId);
+			pstatement.setLong(6, amount);
 			int affectedRows = pstatement.executeUpdate();
 
 			if (affectedRows == 0) {
-				throw new SQLException("Creating transfer failed, no rows affected.");
+				throw new SQLException("Non si dispone dei fondi necessari per effettuare il bonifico");
 			}
 
 			try (ResultSet generatedKeys = pstatement.getGeneratedKeys()) {
