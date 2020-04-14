dd25949c7650ad4c30d1828c2f3dc8d6a8725dbc
Alessio Spagnolo <alessio.spanish@mail.polimi.it>
2020-04-14 15:28:30 +0200

    Create transfer and Templating Transfer page

--- src/it/polimi/tiw/bank/controllers/CreateTransfer.java
+++ src/it/polimi/tiw/bank/controllers/CreateTransfer.java
@@ -27,6 +27,7 @@ import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 import org.thymeleaf.context.WebContext;
+
 /**
  * Servlet implementation class CreateTransfer
  */
@@ -34,100 +35,125 @@ import org.thymeleaf.context.WebContext;
 public class CreateTransfer extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	Connection connection = null;
-    private TemplateEngine templateEngine;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public CreateTransfer() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	private TemplateEngine templateEngine;
 
-    public void init() throws ServletException {
-        try {
-            ServletContext context = getServletContext();
-            String driver = context.getInitParameter("dbDriver");
-            String url = context.getInitParameter("dbUrl");
-            String user = context.getInitParameter("dbUser");
-            String password = context.getInitParameter("dbPassword");
-            Class.forName(driver);
-            connection = DriverManager.getConnection(url, user, password);
-        } catch (ClassNotFoundException e) {
-            throw new UnavailableException("Can't load database driver");
-        } catch (SQLException e) {
-            throw new UnavailableException("Couldn't get db connection");
-        }
-        
-        ServletContext servletContext = getServletContext();
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public CreateTransfer() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public void init() throws ServletException {
+		try {
+			ServletContext context = getServletContext();
+			String driver = context.getInitParameter("dbDriver");
+			String url = context.getInitParameter("dbUrl");
+			String user = context.getInitParameter("dbUser");
+			String password = context.getInitParameter("dbPassword");
+			Class.forName(driver);
+			connection = DriverManager.getConnection(url, user, password);
+		} catch (ClassNotFoundException e) {
+			throw new UnavailableException("Can't load database driver");
+		} catch (SQLException e) {
+			throw new UnavailableException("Couldn't get db connection");
+		}
+
+		ServletContext servletContext = getServletContext();
 		ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
 		templateResolver.setTemplateMode(TemplateMode.HTML);
-        this.templateEngine = new TemplateEngine();
-        this.templateEngine.setTemplateResolver(templateResolver);
-        templateResolver.setSuffix(".html");
-    }
+		this.templateEngine = new TemplateEngine();
+		this.templateEngine.setTemplateResolver(templateResolver);
+		templateResolver.setSuffix(".html");
+	}
 
-    
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-//		doGet(request, response);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession(false);
-		
-		 if (session != null) {
-//	            CustomerDao customerDao = new CustomerDao(connection);
-//	            AccountDao accountDao = new AccountDao(connection);
-	            TransferDao trasferDao = new TransferDao(connection);
-//	            long customerId = (long) session.getAttribute("CUSTOMERID");
-	            long accountId = (long) request.getAttribute("ACCOUNTID");
-	            
-	            try {
-//	                Customer customer = customerDao.findCustomerById(customerId);
-//	                Account account = accountDao.findAccountByAccountId(accountId);
-//	                List<Transfer> transfersList = trasferDao.findByAccountId(accountId);
-	            	
-	                for (Transfer transfer : transfersList) {
-	                	if (transfer.getSourceAccountId() == accountId) {
-	                		depositedMoney += transfer.getAmount();
-	                	} else {
-	                		earnedMoney += transfer.getAmount();
-	                	}
-	                }
-	                
-	                
-	                if (customer == null) {
-	                    session.invalidate();
-	                    response.sendRedirect("/Bank/login");
-	                } else {
-	                	String path = "/Templates/Account/Account.html";
-	            		ServletContext servletContext = getServletContext();
-	            		final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
-	                    ctx.setVariable("accountId", accountId);
-	                    ctx.setVariable("amount", account.getAmount());
-	                    ctx.setVariable("earnedMoney", earnedMoney);
-	                    ctx.setVariable("depositedMoney", depositedMoney);
-	                    ctx.setVariable("transfers", transfersList);
-	                    
-	                    templateEngine.process(path, ctx, response.getWriter());
-	                }
-	            } catch (SQLException e) {
-	                session.invalidate();
-	                response.sendRedirect("/Bank/login");
-	            }
-	        } else {
-	            response.sendRedirect("/Bank/login");
-	        }
-	}
-	}
 
+		if (session != null) {
+			CustomerDao customerDao = new CustomerDao(connection);
+			AccountDao accountDao = new AccountDao(connection);
+			TransferDao transferDao = new TransferDao(connection);
+
+			Long sourceAccountId = Long.getLong(request.getParameter("source-account-id"));
+			Long destinationCustomerId = Long.getLong(request.getParameter("destination-account-id"));
+			Long destinationAccountId = Long.getLong(request.getParameter("destination-account-id"));
+			Long amount = Long.getLong(request.getParameter("amount"));
+			String cause = request.getParameter("cause");
+			long customerId = (long) session.getAttribute("CUSTOMERID");
+
+			if (destinationCustomerId == null || destinationAccountId == null || amount == null || cause == null) {
+				response.sendError(505, "Parameters incomplete");
+				return;
+			}
+
+			try {
+
+				Customer customer = customerDao.findCustomerById(customerId);
+				if (customer == null) {
+					session.invalidate();
+					response.sendRedirect("/Bank/login");
+					return;
+				}
+
+				transferDao.createTransfer(sourceAccountId, destinationAccountId, destinationCustomerId, amount, cause);
+				
+				Account account = accountDao.findAccountByAccountId(sourceAccountId);
+				long updatedAccountAmountEuro = account.getEuros();
+				String updatedAccountAmountCents = account.getCents();
+
+				String path = "/Templates/Transfer/TransferSuccessful.html";
+				ServletContext servletContext = getServletContext();
+				final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+				ctx.setVariable("sourceAccountId", sourceAccountId);
+
+				ctx.setVariable("destinationAccountId", destinationAccountId);
+				ctx.setVariable("destinationCustomerId", destinationCustomerId);
+
+				ctx.setVariable("transferAmountEuros", amount / 100);
+				ctx.setVariable("transferAmountCents", String.format("%02d", amount % 100));
+
+				ctx.setVariable("updatedAccountAmountEuro", updatedAccountAmountEuro);
+				ctx.setVariable("updatedAccountAmountCents", updatedAccountAmountCents);
+
+//				ctx.setVariable("cause", cause);
+
+				templateEngine.process(path, ctx, response.getWriter());
+
+			} catch (SQLException e) {
+				String path = "/Templates/Transfer/TransferFailed.html";
+				ServletContext servletContext = getServletContext();
+				final WebContext ctx = new WebContext(request, response, servletContext, request.getLocale());
+				ctx.setVariable("sourceAccountId", sourceAccountId);
+
+				ctx.setVariable("destinationAccountId", destinationAccountId);
+				ctx.setVariable("destinationCustomerId", destinationCustomerId);
+
+//				ctx.setVariable("cause", cause);
+
+				ctx.setVariable("errorMessage", e.getMessage());
+
+				templateEngine.process(path, ctx, response.getWriter());
+			}
+
+		} else {
+			response.sendRedirect("/Bank/login");
+		}
+	}
 }
