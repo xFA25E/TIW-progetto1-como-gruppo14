7d09ecd731ffae7b2842d25775874919e0d63abb
Alessio Spagnolo <alessio.spanish@mail.polimi.it>
2020-07-07 19:50:29 +0200

    Adding onePage transfers static

--- WebContent/Templates/Home/bank.js
+++ WebContent/Templates/Home/bank.js
@@ -1,8 +1,36 @@
+"use strict";
+
 (function () {
 
 	// page components
 	window.addEventListener("load", () => {
 		document.getElementById("div-header").addEventListener("click", showAnimation);
+		document.getElementById("search-filter").addEventListener("keyup", searchFilterEvent);
+
+		// searchFilterEvent();
+		modalEvent();
+
+		let accountRows = document.getElementsByClassName("tr-account");
+		let transfersRows = document.getElementsByClassName("tr-transfers");
+		for (let i = 0; i < accountRows.length; i++) {
+			accountRows.item(i).addEventListener
+			("click", 
+			function () {
+				let transfers = transfersRows;
+				let displayed = false;
+				function display() {
+					if (displayed) {
+						transfersRows.item(i).style.display = "none";
+						displayed = false
+					} else {
+						transfersRows.item(i).style.display = "contents";
+						displayed = true
+					}
+				}
+				return display;
+			}())
+			
+		}
 		// if (sessionStorage.getItem("username") == null) {
 		// 	window.location.href = "index.html";
 		// } else {
@@ -32,7 +60,7 @@
 		this.reset = function () { ; }
 		this.registerEvents = function () { ; }
 	}
-	
+
 	function PaymentForm() {
 		this.reset = function () { ; }
 		this.registerEvents = function () { ; }
@@ -45,5 +73,50 @@
 		this.addContact = function () { ; }
 	}
 
+	function searchFilterEvent() {
+		var input, filter, table, tr, td, i, txtValue;
+		input = document.getElementById("search-filter");
+		filter = input.value.toUpperCase();
+		table = document.getElementById("customers");
+		tr = table.getElementsByTagName("tr");
+		for (i = 0; i < tr.length; i++) {
+			td = tr[i].getElementsByTagName("td")[0];
+			if (td) {
+				txtValue = td.textContent || td.innerText;
+				if (txtValue.toUpperCase().indexOf(filter) > -1) {
+					tr[i].style.display = "";
+				} else {
+					tr[i].style.display = "none";
+				}
+			}
+		}
+	}
+
+	function modalEvent() {
+		var modal = document.getElementById("modal");
+
+		// Get the button that opens the modal
+		var btn = document.getElementById("btn-address-book");
+
+		// Get the <span> element that closes the modal
+		// var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		btn.onclick = function () {
+			modal.style.display = "flex";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		// span.onclick = function () {
+		// 	modal.style.display = "none";
+		// }
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function (event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		}
+	}
 })();
 
